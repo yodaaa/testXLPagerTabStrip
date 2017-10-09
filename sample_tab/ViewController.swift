6d5bb77516135ec1333631ce9133fd2349fc831e
yodaaa <yodaaa@MacBookProyodaaa.local>
2017-10-09 15:22:23 +0900

    Initial Commit

---
+++ sample_tab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample_tab
+//
+//  Created by yodaaa on 2017/10/09.
+//  Copyright © 2017年 yodaaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
